14c0d2bee60fa20c60d51627cdded06928c78f9b
jitkasem pintaya <jitkasem.pin@ascendcorp.com>
2018-12-09 10:25:48 +0700

    Write to another kafka output test
    For learning purpose

--- spark_read_kafka.py
+++ spark_read_kafka.py
@@ -20,27 +20,41 @@ if __name__ == "__main__":
                         .option("subscribe", topic)\
                         .load()
 
-    tweet_df = tweet_data.selectExpr("CAST(value AS STRING) as tweetmessage")
+    #tweet_df = tweet_data.selectExpr("CAST(value AS STRING) as tweetmessage")
 
-    def add_length_str(text):
+    #def add_length_str(text):
 
-        str_len = len(text)
-        return str_len
+    #    str_len = len(text)
+    #    return str_len
 
 
-    add_length_str_udf = udf(
-                            add_length_str,
-                            IntegerType()
-                            )
+    #add_length_str_udf = udf(
+    #                        add_length_str,
+    #                        IntegerType()
+    #                        )
 
-    tweet_df = tweet_df.withColumn(
-                                    "word_length", 
-                                    add_length_str_udf(tweet_df.tweetmessage)
-                                    )
-    query = tweet_df.writeStream\
-                                .outputMode("append")\
-                                .format("console")\
-                                .option("truncate", "false")\
-                                .trigger(processingTime="5 seconds")\
-                                .start()\
-                                .awaitTermination()
+    #tweet_df = tweet_df.withColumn(
+    #                                "word_length", 
+    #                                add_length_str_udf(tweet_df.tweetmessage)
+    #                                )
+    #query = tweet_df.writeStream\
+    #                            .outputMode("append")\
+    #                            .format("console")\
+    #                            .option("truncate", "false")\
+    #                            .trigger(processingTime="5 seconds")\
+    #                            .start()\
+    #                            .awaitTermination()
+
+    query = tweet_data \
+                                 .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
+                                 .writeStream \
+                                 .format("kafka") \
+                                 .option("kafka.bootstrap.servers", "localhost:9092") \
+                                 .option("topic", "topic_output") \
+                                 .start() \
+                                 .awaitTermination()
+    
+
+
+
+    
