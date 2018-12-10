08945ba34a3a2f2380ebd7bc165e43ead2082f07
jitkasem pintaya <jitkasem.pin@ascendcorp.com>
2018-12-10 18:04:10 +0700

    Fix the scala cosmos db project to be correct
    ............................................

--- scala_spark_to_cosmosdb/README.md
+++ scala_spark_to_cosmosdb/README.md
@@ -1,1 +1,8 @@
-# In progress
+# Kafka-streaming
+Interactive data analysis with Spark streaming session code repo
+
+# Pre-requisite
+
+* Java
+* Scala
+* Maven
--- scala_spark_to_cosmosdb/src/main/scala/com/jitkasem/KafkaSparkCosmosDB.scala
+++ scala_spark_to_cosmosdb/src/main/scala/com/jitkasem/KafkaSparkCosmosDB.scala
@@ -7,7 +7,6 @@ import com.microsoft.azure.cosmosdb.spark.schema._
 import com.microsoft.azure.cosmosdb.spark.config.Config
 import com.microsoft.azure.cosmosdb.spark.streaming._
 
-
 object KafkaSparkCosmosDB {
 
   def main(args: Array[String]): Unit = {
@@ -24,29 +23,31 @@ object KafkaSparkCosmosDB {
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
       .option("subscribe", "topic_output")
-      .option("startingOffsets", "earliest")
+      .option("failOnDataLoss", "false")
       .load()
 
-    val data = df.selectExpr("CAST(key AS STRING) as id", "CAST(value AS STRING) as word_length")
-      .as[(String, String)]
-
-    // COMMAND ----------
+    //  val data = df.selectExpr("CAST(key AS STRING) as id", "CAST(value AS STRING) as word_length")
+    //  .as[(String, String)]
+    //
+    //  COMMAND ----------
 
     val sinkConfigMap = Map(
       "Endpoint" -> "https://jitkasem.documents.azure.com:443/",
       "Masterkey" -> "Y9zV6AJW8QO9M5ey3ZXUtZJ2tmZc1LNHh0pJWxec3rK13quO9kXWT1xolenqYajUYxLLMYG7ABBA4V5QKu34Zw==",
       "Database" -> "maindb",
       "Collection" -> "maindata",
-      "PreferredRegions" -> "Central US;East US2;",
-      "WritingBatchSize" -> "100",
+      "checkpointLocation" -> "checkpoint",
+      "WritingBatchSize" -> "10",
       "Upsert" -> "true")
 
     // Start the stream writer
-    val streamingQueryWriter = data.writeStream
+    val streamingQueryWriter = df.writeStream
       .format(classOf[CosmosDBSinkProvider].getName)
       .outputMode("append")
       .options(sinkConfigMap)
       .start()
+      .awaitTermination()
+
 
   }
 
