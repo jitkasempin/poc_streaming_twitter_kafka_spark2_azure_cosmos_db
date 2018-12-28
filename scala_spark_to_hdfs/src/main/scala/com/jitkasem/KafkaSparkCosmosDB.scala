1b641304e14da4179aa4d482944b44138e83a2c7
jitkasempin <jitkasempintaya@gmail.com>
2018-12-28 02:09:35 +0000

    change to use to read the schema file

--- scala_spark_to_hdfs/src/main/scala/com/jitkasem/KafkaSparkCosmosDB.scala
+++ scala_spark_to_hdfs/src/main/scala/com/jitkasem/KafkaSparkCosmosDB.scala
@@ -13,17 +13,17 @@ object KafkaSparkCosmosDB {
 
   def main(args: Array[String]): Unit = {
 
-    val kafkaUrl = "localhost:9092"
+    val kafkaUrl = "ip-172-31-17-31.ec2.internal:9092"
 
-    val schemaRegistryURL = "http://localhost:8081"
+    val schemaRegistryURL = "http://ip-172-31-17-31.ec2.internal:8081"
 
     val topic = "confluent-in-prices"
 
     val schemaRegistryConfs = Map(
       SchemaManager.PARAM_SCHEMA_REGISTRY_URL          -> schemaRegistryURL,
       SchemaManager.PARAM_SCHEMA_REGISTRY_TOPIC        -> topic,
-      SchemaManager.PARAM_VALUE_SCHEMA_NAMING_STRATEGY -> SchemaManager.SchemaStorageNamingStrategies.TOPIC_RECORD_NAME, // choose a subject name strategy
-      SchemaManager.PARAM_VALUE_SCHEMA_ID              -> "latest" // set to "" if you want the latest schema version to used
+      SchemaManager.PARAM_VALUE_SCHEMA_NAMING_STRATEGY -> SchemaManager.SchemaStorageNamingStrategies.TOPIC_NAME,
+      SchemaManager.PARAM_VALUE_SCHEMA_ID              -> "21" // set to if you want the latest schema version to used
     )
 
     val conf = new SparkConf()
@@ -32,21 +32,16 @@ object KafkaSparkCosmosDB {
 
     val spark = SparkSession.builder.config(conf).getOrCreate()
 
-//    import spark.implicits._
-    // import Spark Avro Dataframes
-//    import za.co.absa.abris.avro.AvroSerDe._
-
     import abris.avro.AvroSerDe._
-
 
     spark.sparkContext.setLogLevel("ERROR")
 
     val df = spark
       .readStream
       .format("kafka")
-      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("kafka.bootstrap.servers", "ip-172-31-17-31.ec2.internal:9092")
       .option("subscribe", topic)
-      .fromConfluentAvro("value", None,  Some(schemaRegistryConfs))(RETAIN_SELECTED_COLUMN_ONLY)
+      .fromConfluentAvro("value", Some("/home/ubuntu/poc_streaming_twitter_to_kafka_to_spark_to_hdfs/scala_spark_to_hdfs/src/main/scala/com/jitkasem/confluent-in-prices-value.avsc"), None)(RETAIN_SELECTED_COLUMN_ONLY)
 
 
     val dss = df.writeStream.format("console").start().awaitTermination()
