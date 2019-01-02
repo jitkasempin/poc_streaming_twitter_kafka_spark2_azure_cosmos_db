4e9b32209eda35ef201bea6ab904c5498662d67f
jitkasem pintaya <jitkasem.pin@ascendcorp.com>
2019-01-02 12:02:44 +0700

    correct the source code
    correct the code

--- scala_spark_to_hdfs/src/main/scala/com/jitkasem/KafkaSparkCosmosDB.scala
+++ scala_spark_to_hdfs/src/main/scala/com/jitkasem/KafkaSparkCosmosDB.scala
@@ -1,7 +1,5 @@
 package com.jitkasem
 
-import abris.avro.read.confluent.SchemaManager
-import abris.avro.schemas.policy.SchemaRetentionPolicies.RETAIN_SELECTED_COLUMN_ONLY
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
@@ -15,24 +13,13 @@ object KafkaSparkCosmosDB {
 
     val kafkaUrl = "ip-172-31-17-31.ec2.internal:9092"
 
-    val schemaRegistryURL = "http://ip-172-31-17-31.ec2.internal:8081"
-
-    val topic = "confluent-in-prices"
-
-    val schemaRegistryConfs = Map(
-      SchemaManager.PARAM_SCHEMA_REGISTRY_URL          -> schemaRegistryURL,
-      SchemaManager.PARAM_SCHEMA_REGISTRY_TOPIC        -> topic,
-      SchemaManager.PARAM_VALUE_SCHEMA_NAMING_STRATEGY -> SchemaManager.SchemaStorageNamingStrategies.TOPIC_NAME,
-      SchemaManager.PARAM_VALUE_SCHEMA_ID              -> "21" // set to if you want the latest schema version to used
-    )
+    val topic = "iot_topic"
 
     val conf = new SparkConf()
                   .setMaster(RUN_LOCAL_WITH_AVAILABLE_CORES)
                   .setAppName(APPLICATION_NAME)
 
     val spark = SparkSession.builder.config(conf).getOrCreate()
-
-    import abris.avro.AvroSerDe._
 
     spark.sparkContext.setLogLevel("ERROR")
 
@@ -41,18 +28,16 @@ object KafkaSparkCosmosDB {
       .format("kafka")
       .option("kafka.bootstrap.servers", "ip-172-31-17-31.ec2.internal:9092")
       .option("subscribe", topic)
-      .fromConfluentAvro("value", Some("/home/ubuntu/poc_streaming_twitter_to_kafka_to_spark_to_hdfs/scala_spark_to_hdfs/src/main/scala/com/jitkasem/confluent-in-prices-value.avsc"), None)(RETAIN_SELECTED_COLUMN_ONLY)
+      .load()
 
-
-    val dss = df.writeStream.format("console").start().awaitTermination()
-
-//                .selectExpr("CAST(value AS STRING)")
-//                .writeStream
-//                .format("csv")
-//                .option("checkpointLocation", "chkpointjittempdir")
-//                .option("path","hdfs://localhost:9000/user")
-//                .start()
-//                .awaitTermination()
+    val dss = df
+                .selectExpr("CAST(value AS STRING)")
+                .writeStream
+                .format("csv")
+                .option("checkpointLocation", "chkpointjittempdir")
+                .option("path","hdfs://localhost:9000/user")
+                .start()
+                .awaitTermination()
 
   }
 
