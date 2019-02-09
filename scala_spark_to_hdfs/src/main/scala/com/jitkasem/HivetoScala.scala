964d0df1e1e7ffe104d7f047014c6d58f761f5a2
jitkasem pintaya <40904509+jitkasempin@users.noreply.github.com>
2019-02-09 08:54:48 +0700

    Update HivetoScala.scala

--- scala_spark_to_hdfs/src/main/scala/com/jitkasem/HivetoScala.scala
+++ scala_spark_to_hdfs/src/main/scala/com/jitkasem/HivetoScala.scala
@@ -13,16 +13,6 @@ object HivetoScala {
 
   def main(args: Array[String]): Unit = {
 
-//    val tableName = "test_hiveserver2"
-
-//    val hiveLocation = "hdfs://dp-hdp-master-1.c.dataplatform-1363.internal:8020/user/hive/warehouse"
-
-//    val spark = SparkSession.builder().appName("SparkHiveExample").master("local[*]").config("spark.sql.warehouse.dir", hiveLocation).config("spark.driver.allowMultipleContexts", "true").getOrCreate()
-
-//    val arraylist: Array[(String, String)] = Array(("id", "dong"), ("x4", "jitkasem"), ("x5", "pintaya"))
-
-//    val rdd = sc.parallelize (arraylist).map (x => Row(x._1, x._2.asInstanceOf[Number].doubleValue()))
-
     val conf = new SparkConf().setAppName(APPLICATION_NAME)
 
     val spark = SparkSession.builder.config(conf).enableHiveSupport().getOrCreate()
@@ -57,7 +47,6 @@ object HivetoScala {
     val empRDD = spark.sparkContext.parallelize(employees,1)
 
     val employeeDataF = empRDD.toDF("first", "second")
-    //    spark.sql("create external table if not exists %s (active_flg string, last_upd_by string,merchant_type_rv string, created long, last_upd long, merchant_name string, merchant_id string, row_id string, created_by string) STORED AS ORC TBLPROPERTIES ('transactional'='true') LOCATION '/user/dong/'".format(tableName)).show()
 
     employeeDataF.show()
 
@@ -70,7 +59,7 @@ object HivetoScala {
       .save()
 
 
-    val df = spark.read.json("hdfs://dp-hdp-master-1.c.dataplatform-1363.internal:8020/user/root/gcs_files/cpg711/ti_cpg_merchant/2017/ti_cpg_merchant_20171006_1.json")
+    val df = spark.read.json("hdfs://abc.internal:8020/user/root/gcs_files/data_1.json")
 
 //    df.write.format("orc").mode("overwrite").insertInto(tableName)
 
